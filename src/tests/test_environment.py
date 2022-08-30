fea333319e12e6271e75e629c039a323de3f7018
Remi <remi.riviere@marcel.cab>
2022-08-30 19:26:49 +0200

    Add test for context and inline environment

--- src/tests/test_environment.py
+++ src/tests/test_environment.py
@@ -22,3 +22,10 @@ def test_env_default(
     env_default,
 ):  # pylint: disable=unused-argument, redefined-outer-name
     assert get_environment_name() == "default"
+
+
+def test_env_context():
+    assert get_environment_name() == "Context_Environment"
+
+def test_env_inline():
+    assert get_environment_name() == "Still another env"
